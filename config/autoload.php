6feebed39afc20b8f14487172540ae023a76b656
Gerald Meier <garyee@gmx.de>
2015-04-28 11:01:28 +0200

    restructure

---
+++ config/autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	// Src
+	'PageAjax' => 'system/modules/page2ajax/src/PageAjax.php',
+));
